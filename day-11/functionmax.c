466c3d237ec0a9dd22abfc853a8300a7024f5f7a
AKSHAT PRATAP SINGH-2029 <akshatpratapsingh942@gmail.com>
2026-06-11 20:35:23 +0530

    Add files via upload

---
+++ day-11/functionmax.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int maximum(int a, int b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
+int main()
+{
+    int x, y, max;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    max = maximum(x, y);
+
+    printf("Maximum number is %d\n", max);
+
+    return 0;
+}
